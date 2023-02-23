8d44dd8d928f1145d1fd5eceb3ecfe0076ea5280
JamaalHarris <jamaalmalikharris@gmail.com>
2023-02-23 17:42:26 -0600

    initial commit

---
+++ inheritance_example.rb
@@ -0,0 +1,54 @@
+class Vehicle
+end
+
+class Car
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+bike1 = Bike.new
+car1 = Car.new
+puts bike1.accelerate
+puts bike1.turn("south")
+puts bike1.ring_bell
---
+++ store_item.rb
@@ -0,0 +1,19 @@
+# puts "Please select one letter [C]reate [R]ead [U]pdate [D]elete [Q]uit:"
+# input = gets.chomp
+
+# if input == "C"
+#   puts "First Name:"
+#   first_name = gets.chomp
+#   puts "First name is #{first_name}"
+#   puts "Last Name:"
+#   last_name = gets.chomp
+#   puts "Last name is #{last_name}"
+#   puts "Salary  amount is: "
+#   salary = gets.chomp.to_s
+#   puts "Salary is #{salary} a year"
+# elsif input == "R"
+#   puts "Name is" + "#{first_name}" + " #{last_name}"
+# elsif input == "U"
+# elsif input == "D"
+# elsif input == "Q"
+# end
